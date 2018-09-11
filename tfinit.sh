8d1ba2e9cfea701a6efcc4fe1c9c9123e45ed34d
Michael (Mike) Williams <34947926+mpw07458@users.noreply.github.com>
2018-09-11 00:58:48 -0400

    Update tfinit.sh

--- tfinit.sh
+++ tfinit.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 terraform init \
-  -backend-config "storage_account_name=tfsstate" \
-  -backend-config="container_name=tfsstate"
+  -backend-config "storage_account_name=cloudshellstoragempw" \
+  -backend-config="container_name=mpwcontainer"
